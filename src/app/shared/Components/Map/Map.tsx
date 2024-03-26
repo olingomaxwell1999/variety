fbaff91e38f48ca65678b86d838ab0906c509579
Olingo Maxwell <olingomaxwell1999@gmail.com>
2024-03-26 06:03:13 -0700

    type checking update

--- src/app/shared/Components/Map/Map.tsx
+++ src/app/shared/Components/Map/Map.tsx
@@ -8,9 +8,9 @@ const Map = () => {
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15954.862509956678!2d36.9016809!3d-1.3470954!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f0d859f97d981%3A0x99c87449f8b0419b!2sVariety%20Flooring%20Works%20Ltd!5e0!3m2!1sen!2ske!4v1711446226978!5m2!1sen!2ske"
         width="100%"
         height="450"
-        allowfullscreen=""
+        allowFullScreen
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
+        referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
     </div>
   );
