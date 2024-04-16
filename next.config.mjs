ad253ce91cbbd238852390141fc37d7cee751da5
Olingo Maxwell <olingomaxwell1999@gmail.com>
2024-04-16 09:01:31 +0300

    domain configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://variety.co.ke/wp-json/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
